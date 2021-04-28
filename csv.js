d01a4f8f45fb85179b5c52fab0397c2389aad603
danj-O <danjowanjo@gmail.com>
2021-04-28 16:49:00 -0400

    adding a cron job file and mongodb integration

--- csv.js
+++ csv.js
@@ -2,8 +2,7 @@ const ObjectsToCsv = require('objects-to-csv');
 
 const createCSV = async(data, batchName) => {
   //data is all the scraped data
-  // console.log("DATA", data)
-
+  console.log("DATA", data)
   // const csv = new ObjectsToCsv(data);
   const csv = new ObjectsToCsv(data);
   // Save to file:
@@ -14,30 +13,11 @@ const createCSV = async(data, batchName) => {
 
 
 const createErrorCSV = async(data, batchName) => {
-  let issueData = []
-  data.map(item => {
-    console.log('availablitilty', item.availability, item.availability !== 'In Stock.')
-    if(item.price == 'NULL' || item.buyBox == 'NULL' || item.shipsFrom == 'NULL' || item.availability !== 'In Stock.'){
-      issueData.push({
-        asin: item.asin,
-        title: item.title,
-        price: item.price,
-        buybox: item.buyBox,
-        shipsFrom: item.shipsFrom,
-        availability: item.availability
-      })
-    }
-  })
-  //data is all the scraped data
-  // console.log("DATA", data)
-
-  // const csv = new ObjectsToCsv(data);
-  const csv = new ObjectsToCsv(issueData);
+  const csv = new ObjectsToCsv(data);
   // Save to file:
   await csv.toDisk(`./${batchName}-issues.csv`);
-  // Return the CSV file as string:
-  // console.log(await csv.toString());
 }
+
 
 module.exports = {
   createCSV: createCSV,
